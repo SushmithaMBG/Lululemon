929668f0a739f69dcf2964e5deb67dc41263c17f
sushmithag <sushmithag@192.168.9.47>
2025-09-04 12:39:39 +0530

    BRANCH COMMIT

--- GIt_Demo/src/GIt_Demo/Welcome.java
+++ GIt_Demo/src/GIt_Demo/Welcome.java
@@ -8,6 +8,7 @@ public class Welcome {
 		System.out.println("Pulling the code to gitHub");
 		System.out.println("Pushingg the code to gitHub attempt 2");
 		System.out.println("Pushingg TO BRANCH 1");
+		System.out.println("Pushingg TO BRANCH ");
 		
 	}
 //https://github.com/SushmithaMBG/Lululemon.git
